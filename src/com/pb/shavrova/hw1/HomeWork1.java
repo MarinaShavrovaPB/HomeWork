4be6ed6d9ecd427e9b77c9b5b789a033ab5380d1
Шаврова Марина Петровна <maryna.shavrova@privatbank.ua>
2021-10-08 19:49:12 +0300

    test

--- src/com/pb/shavrova/hw1/HomeWork1.java
+++ src/com/pb/shavrova/hw1/HomeWork1.java
@@ -2,7 +2,7 @@ package com.pb.shavrova.hw1;
 
 public class HomeWork1 {
     public static void main(String[] args){
-        System.out.println("Hello! Word");
+        System.out.println("Hello! World!");
     }
 }
 
